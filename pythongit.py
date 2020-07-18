ab9625a865c1803dad76db94b197022f9eb8ee70
Lobsang <lobsangmercedes@outlook.com>
2020-07-18 17:25:55 -0400

    elecciones completas

--- pythongit.py
+++ pythongit.py
@@ -6,7 +6,16 @@ def run():
     print("3.Dolar a peso dominicano")
     print("4.Euro a peso dominicano")
     opcion = int(input("Ingresa la opcion: "))
-        
+    if opcion == 1:
+        peso_a_dolar()
+    elif opcion == 2:
+        peso_a_euro()
+    elif opcion == 3:
+        dolar_a_peso()
+    elif opcion == 4:
+        euro_a_peso()
+
+    
 
 
 

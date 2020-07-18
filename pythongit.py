cb4f91f98428dd2af2598f1b58b31ac11ea2ce3b
Lobsang <lobsangmercedes@outlook.com>
2020-07-18 17:21:48 -0400

    Opciones agregadas

--- pythongit.py
+++ pythongit.py
@@ -1,11 +1,12 @@
 
 def run():
     print("Esta es una calculadora para divisas")
-
-
-
-
-
+    print("1.Peso Dominicano a dolar")
+    print("2.Peso Dominicano a euro")
+    print("3.Dolar a peso dominicano")
+    print("4.Euro a peso dominicano")
+    opcion = int(input("Ingresa la opcion: "))
+        
 
 
 

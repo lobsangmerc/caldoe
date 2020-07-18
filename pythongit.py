2ab534e6fe519948239acdc26b6138aa18058a84
Lobsang <lobsangmercedes@outlook.com>
2020-07-18 17:53:05 -0400

    prueba de funciones

--- pythongit.py
+++ pythongit.py
@@ -1,4 +1,22 @@
 
+dolar = float(58.56)
+euro = float(66.86)
+
+
+
+
+def peso_a_dolar():
+    cantidad = int(input('Ingrese la cantidad que desea convertir: '))
+    total = float(cantidad*dolar)
+    print(total)
+
+
+
+
+
+
+
+
 def run():
     print("Esta es una calculadora para divisas")
     print("1.Peso Dominicano a dolar")
@@ -8,12 +26,13 @@ def run():
     opcion = int(input("Ingresa la opcion: "))
     if opcion == 1:
         peso_a_dolar()
-    elif opcion == 2:
-        peso_a_euro()
-    elif opcion == 3:
-        dolar_a_peso()
-    elif opcion == 4:
-        euro_a_peso()
+
+    #elif opcion == 2:
+        #peso_a_euro()
+   # elif opcion == 3:
+        #dolar_a_peso()
+    #elif opcion == 4:
+        #euro_a_peso()
 
     
 
